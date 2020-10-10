2ba304f61c07b189dd423e439efb25cb373f04c4
Hakon Olsen <haakools@student.geo.uio.no>
2020-10-10 18:57:23 +0200

    added documentation to the functions.py script

--- Project 1/functions.py
+++ Project 1/functions.py
@@ -11,29 +11,53 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 
 def FrankeFunction(x,y,noisefactor):
-        term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-        term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-        term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-        term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-        term5 = noisefactor*np.random.normal(0,1,(len(x),len(y)))
-        return term1 + term2 + term3 + term4 + term5
+    """
+    Input:
+        x  : x part of meshgrid of x,y
+        y  : y part of meshgrid of x,y
+        noisefactor : magnitude of the noise. 
+        
+    Computes the frankefunction on a meshgrid with added noise, if noisefactor >0, and returns it 
+    as a meshgrid.
+    Output:
+        FrankeFunction : (N,N) dataset of the franke function. 
+    """
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    term5 = noisefactor*np.random.normal(0,1,(len(x),len(y)))
+    return term1 + term2 + term3 + term4 + term5
         
 
 def create_X(x, y, n ):
-        if len(x.shape) > 1:
-                x = np.ravel(x)
-                y = np.ravel(y)
+    """
+    Inputs:
+        x  : x part of meshgrid of x,y
+        y  : y part of meshgrid of x,y
+        n  : Polymomial degree of the fit, (x+y)^n
+    Creates the design matrix, where the columns are ordered as
+    [1, x,y,x^2,xy,y^2,x^3,x^2y, ..., xy^(n-1), y^(n)]
+    
+    Outputs:
+        X  : Design matrix (N,l) where l=(n+1)(n+2)/2
+    """
+    
+    
+    if len(x.shape) > 1:
+            x = np.ravel(x)
+            y = np.ravel(y)
 
-        N = len(x)
-        l = int((n+1)*(n+2)/2)          # Number of elements in beta                                                               
-        X = np.ones((N,l))
+    N = len(x)
+    l = int((n+1)*(n+2)/2)          # Number of elements in beta                                                               
+    X = np.ones((N,l))
 
-        for i in range(1,n+1):
-                q = int((i)*(i+1)/2)
-                for k in range(i+1):
-                        X[:,q+k] = (x**(i-k))*(y**k)
+    for i in range(1,n+1):
+            q = int((i)*(i+1)/2)
+            for k in range(i+1):
+                    X[:,q+k] = (x**(i-k))*(y**k)
 
-        return X
+    return X
 
 def init_data(N,noisefactor):
     """
@@ -53,37 +77,36 @@ def init_data(N,noisefactor):
     return x_, y_, z    
 
 def Scaling(X_train, X_test):
-        (N, p) = X_train.shape
-        if p > 1:
-            scaler = StandardScaler()
-            scaler.fit(X_train[:,1:])
-            X_train = scaler.transform(X_train[:,1:])
-            X_test = scaler.transform(X_test[:,1:])
-            
-            # Adding the intercept after the scaling, as the StandardScaler removes the 1s in the first column.
-            intercept_train = np.ones((len(X_train),1))
-            intercept_test = np.ones((len(X_test),1))
-            X_train = np.concatenate((intercept_train,X_train),axis=1)
-            X_test = np.concatenate((intercept_test,X_test),axis=1)
-        else:
-            X_train = X_train
-            X_test = X_test
-        return X_train, X_test
-
-def X_scaling(X):
-        (N, p) = X.shape
-        if p > 1:
-            scaler = StandardScaler()
-            scaler.fit(X[:,1:])
-            X = scaler.transform(X[:,1:])            
-            # Adding the intercept after the scaling, as the StandardScaler removes the 1s in the first column.
-            intercept = np.ones((len(X),1))
-            X = np.concatenate((intercept,X),axis=1)
-
-        else:
-            X = X
-        return X
     
+    """ 
+    Input:
+        X_train   : Training part of the design matrix
+        X_test    : testing part of the design matrix
+    
+    Scales the Design matrix with standard scaler. Removes the intercept as it is just 1s and adds it after the scaling.
+    
+    Output:
+        X_train   : Training design matrix scaled
+        X_test   : Testing design matrix scaled
+    """
+    (N, p) = X_train.shape
+    if p > 1:
+        scaler = StandardScaler()
+        scaler.fit(X_train[:,1:])
+        X_train = scaler.transform(X_train[:,1:])
+        X_test = scaler.transform(X_test[:,1:])
+
+        # Adding the intercept after the scaling, as the StandardScaler removes the 1s in the first column.
+        intercept_train = np.ones((len(X_train),1))
+        intercept_test = np.ones((len(X_test),1))
+        X_train = np.concatenate((intercept_train,X_train),axis=1)
+        X_test = np.concatenate((intercept_test,X_test),axis=1)
+    else:
+        X_train = X_train
+        X_test = X_test
+    return X_train, X_test
+
+
 def PreProcess(x, y,z, test_size, n):
     """
     Input:
@@ -114,17 +137,43 @@ def PreProcess(x, y,z, test_size, n):
     
 
 def MSE(y,ypred):
+    """
+    Input:
+        y      :  The true data
+        ypred  :  The predicted data
+    Computes the MSE error
+    Output:
+        MSE : Mean Squared Error
+    """
     MSE = np.mean((y-ypred)**2)
     return MSE
 
 def R2(y,ypred):
+    """
+    Input:
+        y        : The true data
+        ypred    : The predicted data
+    Computes the R2 score
+    Output:
+        MSE      : Mean Squared Error
+        """
     return 1-np.sum((y-ypred)**2)/np.sum((y-np.mean(y))**2)
 
 
 def ErrBiasVar(z_test,z_pred):
     """
+    Input:
+        z_test   : The testing data
+        z_pred   : The predicted test data
+ 
     Takes in corresponding test and predicted data and computes the 
     error, bias and variance
+    
+    The true function is usually not know, and so we are substituting f_i with the test data (including the noise)
+    Output:
+        error      : MSE 
+        bias^2     : squared bias of the test data
+        variance   : variance of the predicted test data
     """
     error = np.mean( np.mean((z_test - z_pred)**2, axis=1, keepdims=True) )
     bias  = np.mean( (z_test - np.mean(z_pred, axis=1, keepdims=True))**2 )
@@ -144,6 +193,20 @@ def SVDinv(A):
     return np.matmul(V,np.matmul(invD,UT))
 
 def Shuffle_Data(x,z, replacement=True):
+    """
+    Input:
+        x   : design matrix
+        z   : data
+ 
+    Shuffles the the design matrix and the data equally wrt. the rows. If replecament is true,
+    the same data points can be sampled twice or more, which
+    is incorporated into the bootstrapping.
+    
+    Output:
+        x   : design matrix shuffled
+        z   : data shuffled
+    """
+
     if replacement == True:
         size = len(x)
         index = np.random.randint(1, size, size=size)
@@ -168,8 +231,23 @@ def k_foldsplit(N, folds):
         foldmask[:,j] = mask
     return foldmask
 
-def surfplotter(x,y,BETA,n, title):
-
+def surfplotter(x,y,BETA,n, title, zlim):
+    """
+    Input:
+        x    : Linspace of the x dimension (0,1,N)
+        y    : Linspace of the y dimension (0,1,N)
+        BETA : Beta coefficients of the fit
+        n    : polynomial degree
+        title: title of the figure
+        zlim : the z limits of the figure
+        
+    Plots the prediction of the estimated betacoefficient over x,y, i.e. trained on training data and 
+    plotted on the whole range of values.
+    
+    Output: 
+        Surf Plot of the prediction from the betacoefficient
+    
+    """
    
     z_total = create_X(x,y,n) @ BETA
     z_total = z_total.reshape(x.shape)
@@ -187,11 +265,33 @@ def surfplotter(x,y,BETA,n, title):
     ax.zaxis.set_major_locator(LinearLocator(10))
     ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
     ax.set_title(title, fontsize = 16)
-    ax.set_zlim(-0.10, 1.40)
+    ax.set_zlim(zlim)
 
     # Add a color bar which maps values to colors.
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
+
+
+
+
+
+def terrain_sampling(x,y,z):
+    """
+    Samples down the terrain data to fit the length of x and y,
+    effectively reducing the resolution of the data.
+    """
+    z_sample = np.zeros((len(x),len(y)))
+    zx, zy = z.shape
+    
+    for i in range(len(x)):
+        for j in range(len(y)):
+            z_sample[i,j] = z[np.int(zx/len(x))*i, np.int(zy/len(y))*j]
+    return z_sample
+
+"""
+PREVIOUSLY USED FUNCTION BUT NOT DOCUMENTED BELOW
+
+"""
 
 
 
@@ -206,22 +306,22 @@ def betaplot(beta, title):
 
     plt.show()
 
-def terrainInit(filename, x_start, x_end, y_start, y_end):
-    from imageio import imread
-    terrain1 = imread(filename)
-    # Initializing the data
-    z = np.array(terrain1[x_start:x_end, y_start:y_end])
-    return z
 
-def terrain_sampling(x,y,z):
-    """
-    Samples down the terrain data to fit the length of x and y,
-    effectively reducing the resolution of the data.
-    """
-    z_sample = np.zeros((len(x),len(y)))
-    zx, zy = z.shape
+
+def X_scaling(X):
     
-    for i in range(len(x)):
-        for j in range(len(y)):
-            z_sample[i,j] = z[np.int(zx/len(x))*i, np.int(zy/len(y))*j]
-    return z_sample
+    
+    
+        (N, p) = X.shape
+        if p > 1:
+            scaler = StandardScaler()
+            scaler.fit(X[:,1:])
+            X = scaler.transform(X[:,1:])            
+            # Adding the intercept after the scaling, as the StandardScaler removes the 1s in the first column.
+            intercept = np.ones((len(X),1))
+            X = np.concatenate((intercept,X),axis=1)
+
+        else:
+            X = X
+        return X
+    
